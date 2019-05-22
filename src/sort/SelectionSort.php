509b6f75e7c583a82bfe1b10a5155714981dc7e9
Sakerik <e_sakamoto@dipross.jp>
2019-05-22 12:45:15 +0900

    処理のループ作成からソート処理完成まで作成しました。 一先ず完成しましたが、二重ループで説明していたとお聞きしたので今回作成した処理はコメントアウトして、minを使わない処理を以後作成します。

--- src/sort/SelectionSort.php
+++ src/sort/SelectionSort.php
@@ -10,28 +10,23 @@ class SelectionSort {
    */
   function sort($ary){
     // TODO 選択ソートで並び替える処理を実装してください。
+    
+    for($j = 0; $j < count($ary)-1; $j++){
 
-    // ループ用の変数を用意
-    $count = count($ary)-1;
-    $j = 0;
-    
-    // 最小値を検索するための検索範囲を指定する
-    $searchRange = array_slice($ary, $j);
-    
-    // 最小値の検索
-    $min = array_search(min($searchRange), $ary);
-    echo $min;
-    
-    // 検索した最小値が検索範囲内で最小の位置にないのであれば位置を交換する
-    if($ary[$min] != $j){
-      $temp = $ary[$min];
-      $ary[$min] = $ary[$j];
-      $ary[$j] = $temp;
+      // 最小値を検索するための検索範囲を指定する
+      $searchRange = array_slice($ary, $j);
+      
+      // 最小値の検索
+      $min = array_search(min($searchRange), $searchRange) + $j;
+      
+      // 検索した最小値が検索範囲内で最小の位置にないのであれば位置を交換する
+      if($ary[$min] != $ary[$j]){
+        $temp = $ary[$min];
+        $ary[$min] = $ary[$j];
+        $ary[$j] = $temp;
+      }
+
     }
-
-    // for($j = 0; $j < $count; $j++){
-    
-    // }
 
     return $ary;
   }
